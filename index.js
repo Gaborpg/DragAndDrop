28abee9cd449a81a8b5d8ff6ae33a45d4ee00d40
Gaborpg <gaborpg@gmail.com>
2019-01-18 17:06:41 +0000

    final

--- index.js
+++ index.js
@@ -1,69 +1,80 @@
 let picked;
 let colorPick;
 let toggleSwitch = false;
-let elements = [...document.getElementById('target').getElementsByTagName('div')];
+let elements = [...document.getElementById('target').children];
 let shapes = [];
-
-
-
+let myVar;
 let square = {
-    startX: 20,
-    startY: 20,
-    drawX: 0,
-    drawY: 0,
-    width: 40,
-    height: 40,
-    fillColor: "#FF0000",
-    originalFillColor: "#FF0000",
-    isMoving: false
+    fillColor: "rgb(255, 56, 56)",
+    setColor: "rgb(255, 242, 0)",
+    originalFillColor: "rgb(255, 56, 56)",
 };
 
 
-let regtangle = {
-    startX: 20,
-    startY: 20,
-    drawX: 0,
-    drawY: 0,
-    width: 40,
-    height: 40,
-    fillColor: "#FF0000",
-    originalFillColor: "#FF0000",
-    isMoving: false
+let rectangle = {
+    fillColor: "rgb(255, 56, 56)",
+    setColor: "rgb(205, 132, 241)",
+    originalFillColor: "rgb(255, 56, 56)",
 };
+
+let triangle = {
+    fillColor: "rgb(255, 56, 56)",
+    setColor: "rgb(24, 220, 255)",
+    originalFillColor: "rgb(255, 56, 56)",
+};
+
+
+(function init() {
+    shapes = [];
+    for (let i = 0; i < elements.length; i++) {
+        shapes.push(elements[i].id);
+    }
+    intervalSet();
+
+})();
+
+
+function intervalSet() {
+    myVar = setInterval(blink, 2000);
+}
+
+
+async function blink() {
+    for (let element of shapes) {
+        await changeColor(element);
+    }
+}
+
+function changeColor(element) {
+    if (element === "triangle") {
+        eval(element).fillColor = eval(element).setColor;
+        document.getElementById("triangleSVG").style.fill = (eval(element).fillColor === document.getElementById("triangleSVG").style.fill) ? 'white' : eval(element).setColor;
+    } else {
+        eval(element).fillColor = eval(element).setColor;
+        document.getElementById(element).style.backgroundColor = (eval(element).fillColor === document.getElementById(element).style.backgroundColor) ? 'white' : eval(element).setColor;
+    }
+
+};
+
 
 function togglebtn() {
-    if (toggleSwitch){
+    if (toggleSwitch) {
         document.getElementById("btn-on").disabled = true;
         document.getElementById("btn-off").disabled = false;
         toggleSwitch = false;
         console.log('ON');
+        intervalSet();
+
     } else {
         document.getElementById("btn-on").disabled = false;
         document.getElementById("btn-off").disabled = true;
         toggleSwitch = true;
         console.log('OFF');
+        clearTimeout(myVar);
 
     }
 
 }
-
-(function blink() {
-    if (!toggleSwitch){
-
-        for (let i= 0; i < elements.length; i++){
-            shapes.push(elements[i].className);
-
-        }
-        setInterval(()=>{}
-
-
-        ,2000);
-        console.log(shapes);
-    }
-})();
-
-
-
 
 
 function dragstart_handler(ev) {
@@ -81,45 +92,53 @@ function dragover_handler(ev) {
 function drop_handler(ev) {
     ev.preventDefault();
 
-        const style = getComputedStyle(colorPick);
-        ev.target.style.backgroundColor = style.backgroundColor;
+    const style = getComputedStyle(colorPick);
 
-        ev.target.className.fillColor = style.backgroundColor;
+    if (ev.target.nodeName === "polygon") {
+        ev.target.parentElement.style.fill = style.backgroundColor;
+        eval(ev.target.parentElement.parentElement.id).setColor = style.backgroundColor;
+    } else {
+        ev.target.style.backgroundColor = style.backgroundColor;
+        eval(ev.target.className).setColor = style.backgroundColor;
+    }
+
 }
 
 
-elements.forEach((element)=> {element.onmousedown = function (event) {
+elements.forEach((element) => {
+    element.onmousedown = function (event) {
 
-    let shiftX = event.clientX - element.getBoundingClientRect().left;
-    let shiftY = event.clientY - element.getBoundingClientRect().top;
+        let shiftX = event.clientX - element.getBoundingClientRect().left;
+        let shiftY = event.clientY - element.getBoundingClientRect().top;
 
-    element.style.position = 'absolute';
-    element.style.zIndex = 1000;
-    document.body.append(element);
+        element.style.position = 'absolute';
+        element.style.zIndex = 1000;
+        document.body.append(element);
 
-    moveAt(event.pageX, event.pageY);
-
-    function moveAt(pageX, pageY) {
-        element.style.left = pageX - shiftX + 'px';
-        element.style.top = pageY - shiftY + 'px';
-    }
-
-    function onMouseMove(event) {
         moveAt(event.pageX, event.pageY);
-    }
 
-    document.addEventListener('mousemove', onMouseMove);
+        function moveAt(pageX, pageY) {
+            element.style.left = pageX - shiftX + 'px';
+            element.style.top = pageY - shiftY + 'px';
+        }
 
-    element.onmouseup = function () {
-        document.removeEventListener('mousemove', onMouseMove);
-        element.onmouseup = null;
+        function onMouseMove(event) {
+            moveAt(event.pageX, event.pageY);
+        }
+
+        document.addEventListener('mousemove', onMouseMove);
+
+        element.onmouseup = function () {
+            document.removeEventListener('mousemove', onMouseMove);
+            element.onmouseup = null;
+        };
+
     };
-
-};
 
     element.ondragstart = function () {
         return false;
-    };});
+    };
+});
 
 
 
